716ee6d2186834d9e0ddef0b93d102012605e121
IvannaBesarab <ibesarab1993@gmail.com>
2015-04-23 21:44:16 +0300

    Removed ignored files with git rm -r -f --cached .  and  after git add .  Hope, it worked good

--- cit/__init__.py
+++ cit/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, request, g
 # Import SQLAlchemy
 from flask.ext.sqlalchemy import SQLAlchemy
 
+#Flask-Admin Initialization
+from flask.ext.admin import Admin, BaseView, expose
+
 # Define the WSGI application object
 app = Flask(__name__)
 
@@ -19,20 +22,17 @@ def index():
     return render_template('index.html',site_title=app.config["SITE_TITLE"])
 
 
-#Flask-Admin Initialization
-from flask.ext.admin import Admin, BaseView, expose
-
-class MyView(BaseView):
+class AdminPageView(BaseView):
     @expose('/')
-    def index(self):
+    def index(self): 
         return self.render('index2.html') 
 # created index2.html alongwith index.html in folder template
 
 admin = Admin(app)
-admin.add_view(MyView(name='Hello'))
-admin.add_view(MyView(name='Hello 1', endpoint='test1', category='Test'))
-admin.add_view(MyView(name='Hello 2', endpoint='test2', category='Test'))
-admin.add_view(MyView(name='Hello 3', endpoint='test3', category='Test'))
+admin.add_view(AdminPageView(name='Hello'))
+admin.add_view(AdminPageView(name='Hello 1', endpoint='test1', category='Test'))
+admin.add_view(AdminPageView(name='Hello 2', endpoint='test2', category='Test'))
+admin.add_view(AdminPageView(name='Hello 3', endpoint='test3', category='Test'))
 
 
 
