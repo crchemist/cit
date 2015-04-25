853be7ca3bc57b3be84500ffbb4b0c58737b6126
Mykola Kharechko <crchemist@gmail.com>
2015-04-25 22:37:34 +0300

    added redirect to / after fb login.

--- cit/auth/controllers.py
+++ cit/auth/controllers.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response, g
+from flask import redirect, render_template, request, make_response, g
 from flask import Blueprint
 
 import authomatic
@@ -13,4 +13,6 @@ auth_bp = Blueprint('auth', __name__)
 def login():
     response = make_response()
     result = g.authomatic.login(WerkzeugAdapter(request, response), 'fb')
+    if result and not result.error:
+        return redirect('/')
     return response
