ca63ddfd7b2bb4c924d7c22dd5d4928048aaf85b
Mykola Kharechko <crchemist@gmail.com>
2015-04-26 12:03:17 +0300

    switch to flask sessions.

--- cit/auth/controllers.py
+++ cit/auth/controllers.py
@@ -1,5 +1,5 @@
 from flask import redirect, render_template, request, make_response, g
-from flask import Blueprint
+from flask import Blueprint, session
 
 import authomatic
 from authomatic.adapters import WerkzeugAdapter
@@ -9,10 +9,15 @@ from .models import User
 
 auth_bp = Blueprint('auth', __name__)
 
+def _session_saver():
+    session.modified = True
+
 @auth_bp.route('/login/fb/', methods=['GET', 'POST'])
 def login():
     response = make_response()
-    result = g.authomatic.login(WerkzeugAdapter(request, response), 'fb')
+    result = g.authomatic.login(WerkzeugAdapter(request, response), 'fb',
+                                session=session,
+                                session_saver=_session_saver)
     if result:
         if result.user:
             return redirect('/')
