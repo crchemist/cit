309a831d0f3f7ae1bd967172c7e8a46d42c146dd
IvanHreskiv <ivanhreskiv@gmail.com>
2015-05-01 08:27:27 -0700

    wsgi.py has being complited

---
+++ wsgi.py
@@ -0,0 +1,18 @@
+import os 
+
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+from cit import app as application
+
+if __name__ == '__main__':
+	from wsgiref.simple_server import make_server
+	ip = os.environ['OPENSHIFT_PYTHON_IP']
+	port = int(os.getenv('OPENSHIFT_PYTHON_PORT'))
+	httpd = make_server(ip, port, application)
+	#Wait for a single request, serve it and quit.
+	httpd.serve_forever()
