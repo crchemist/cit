80caa79f631c04609dae10646b6a0092742bef60
mdemitc <mister_dream@mail.ru>
2015-05-01 01:49:31 -0700

    t_34:fixed_bag_front_end

--- cit/static/js/user_directive.js
+++ cit/static/js/user_directive.js
@@ -3,6 +3,7 @@ app.controller('UserNS', ['$scope', '$http', function($scope, $http) {
         $http.get('/auth/user-info/').success(function(data) {
             $scope.products = data;
             if (Object.keys($scope.products).length > 0) {
+
                 $scope.products.fullNS = $scope.products.name + ' ' + $scope.products.surname;
             } else {
                 $scope.products.fullNS = '';
