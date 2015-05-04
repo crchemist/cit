0c662a3a3c9614b50a642d33470004be87a6925c
zimoham <zimoham@gmail.com>
2015-05-05 00:17:15 +0300

    my version without Alexsandr's correction

--- cit/static/js/directives.js
+++ cit/static/js/directives.js
@@ -5,23 +5,26 @@ directive('errorDirective',['$rootScope', '$location',function($rootScope, $loca
         template: "<div ng-transclude>></div>",
         transclude:true,
         link: function($scope, element, attrs){
-        	
-        	//var termKey = "msg";
         	$scope.showMessage = true;
 
     		$scope.closeAlert = function(){
     			$scope.showMessage = true;
     		}
 
-        	$scope.$watch(function () { return $location.search().msg; }, function() {
-			$scope.msg = $location.search().msg ;
+        	$scope.$watch(function () { return $location.absUrl(); }, function() {
+                if($location.search().msg){
+                    $scope.msg = $location.search().msg;
+                    $scope.showMessage =false;
+                }
+               // $scope.msg = $location.search().msg ;
     		});
-     
+    /* 
 		$scope.$watch('msg', function(msg) {
 			if(msg){
 				$scope.showMessage =false;
 			     }
             })
+*/
         }
     }
 }]);
