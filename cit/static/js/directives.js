82ae5a8ac8345cdda6bf70ba0620252956b5a626
zimoham <zimoham@gmail.com>
2015-05-04 13:19:33 +0300

    optimized code (trash was deleted)

--- cit/static/js/directives.js
+++ cit/static/js/directives.js
@@ -6,25 +6,20 @@ directive('errorDirective',['$rootScope', '$location',function($rootScope, $loca
         transclude:true,
         link: function($scope, element, attrs){
         	
-        	var termKey = "msg";
+        	//var termKey = "msg";
         	$scope.showMessage = true;
 
     		$scope.closeAlert = function(){
     			$scope.showMessage = true;
     		}
 
-
-
-        	$scope.$watch(function () { return $location.search(); }, function() {
-				if ($location.search()[termKey]) 
-					$scope.msg = $location.search()[termKey] ;
-
+        	$scope.$watch(function () { return $location.search().msg; }, function() {
+			$scope.msg = $location.search().msg ;
     		});
      
-		    $scope.$watch('msg', function(msg) {
-		       $location.search(termKey, msg);
-		       if(msg){ 
-				    $scope.showMessage =false;
+		$scope.$watch('msg', function(msg) {
+			if(msg){
+				$scope.showMessage =false;
 			     }
             })
         }
