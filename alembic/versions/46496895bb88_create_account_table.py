ea0b101815d0bc8079f5171cb3d7b14a86aa287f
therobinbad <therobinbad@gmail.com>
2015-05-05 17:52:28 +0300

    Alembic migration tool for SQLAlchemy added.

---
+++ alembic/versions/46496895bb88_create_account_table.py
@@ -0,0 +1,22 @@
+"""create account table
+
+Revision ID: 46496895bb88
+Revises: None
+Create Date: 2015-05-05 17:43:55.923452
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '46496895bb88'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
