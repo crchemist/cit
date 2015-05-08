225b02247ff4329f709517d4c438c70770aca519
zimoham <zimoham@gmail.com>
2015-05-08 22:14:23 +0300

    show error message

--- cit/static/js/directives.js
+++ cit/static/js/directives.js
@@ -1,27 +1,23 @@
-app.
-directive('errorDirective',['$rootScope', '$location',function($rootScope, $location){
+app.directive('errorDirective',['$location',function($location) {
 	return {
         restrict: 'AEC',
         template: "<div ng-transclude>></div>",
         transclude:true,
         link: function($scope, element, attrs){
-        	
-        	//var termKey = "msg";
-        	$scope.showMessage = true;
+        	$scope.hideMessage = true;
 
     		$scope.closeAlert = function(){
-    			$scope.showMessage = true;
+    			$scope.hideMessage = true;
     		}
 
-        	$scope.$watch(function () { return $location.search().msg; }, function() {
-			$scope.msg = $location.search().msg ;
-    		});
-     
-		$scope.$watch('msg', function(msg) {
-			if(msg){
-				$scope.showMessage =false;
-			     }
-            })
-        }
-    }
-}]);
+        	$scope.$watch(function() { 
+				return $location.absUrl(); }, 
+			function() {
+				if($location.search().msg) {
+				    $scope.msg = $location.search().msg;
+				    $scope.hideMessage =false;
+				}              
+     		});
+            }
+         }
+ }]);
