c2bf4ca1ad15d122b4f0b082d0648b72f7d9446c
IvanHreskiv <ivanhreskiv@gmail.com>
2015-05-09 03:35:44 -0700

    fixed wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,12 +7,13 @@ try:
 except IOError:
     pass
 
-from cit import app as application
+from cit import create_app
 
 if __name__ == '__main__':
 	from wsgiref.simple_server import make_server
 	ip = os.environ['OPENSHIFT_PYTHON_IP']
 	port = int(os.getenv('OPENSHIFT_PYTHON_PORT'))
+	application = create_app() 	
 	httpd = make_server(ip, port, application)
 	#Wait for a single request, serve it and quit.
 	httpd.serve_forever()
