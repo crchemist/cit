6deb0c9f1ca0caf00140d76bd2e986b7b551c545
mdemitc <mister_dream@mail.ru>
2015-05-10 04:43:08 -0700

    t_78-new table

--- cit/issues/models.py
+++ cit/issues/models.py
@@ -3,6 +3,7 @@ from ..auth.models import User
 #from geoalchemy2 import Geometry
 
 class Issues(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
 	reporter = db.Column(db.Integer, db.ForeignKey("user.id"))
 	description = db.Column(db.String(120))
 	coordinates = db.Column(db.String(120))
