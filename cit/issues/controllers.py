484a0f1e9221053bc152b2666bfdc9699f0b14a3
IvanHreskiv <ivanhreskiv@gmail.com>
2015-05-12 07:38:14 -0700

    delete if statement from upload_file

--- cit/issues/controllers.py
+++ cit/issues/controllers.py
@@ -10,10 +10,9 @@ UPLOAD_FOLDER = os.path.join(BASE_DIR, 'media')
 
 @issues_bp.route('/file-upload/', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
-		file = request.files['file']
-		if file:
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(UPLOAD_FOLDER, filename))
-
+    file = request.files['file']
+    if file:
+    	filename = secure_filename(file.filename)
+    	file.save(os.path.join(UPLOAD_FOLDER, filename))
+    
 	return redirect('/')
