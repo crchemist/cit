ada63d16e36c18a8ae26471277ba14a078b49a24
zimoham <zimoham@gmail.com>
2015-05-17 23:54:49 +0300

    Revert "add profile"
    This reverts commit a752fa23308b6a8242c27c883af665d568f56ea0.

--- cit/static/js/gallery.js
+++ cit/static/js/gallery.js
@@ -1,17 +1,15 @@
-$(window).load(function(){
-	 $('.slider-for').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  arrows: false,
-	  fade: true,
-	  asNavFor: '.slider-nav'
-	});
-	$('.slider-nav').slick({
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  asNavFor: '.slider-for',
-	  dots: true,
-	  centerMode: true,
-	  focusOnSelect: true
-	});
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
 });
