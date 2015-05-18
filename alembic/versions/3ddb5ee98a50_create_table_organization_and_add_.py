5988c7bc4b0c9d20890f4cd5c1ba0d0d6810e2e5
IvanHreskiv <ivanhreskiv@gmail.com>
2015-05-18 09:27:47 -0700

    fail

---
+++ alembic/versions/3ddb5ee98a50_create_table_organization_and_add_.py
@@ -0,0 +1,22 @@
+"""create table organization and add foreign key organization to user
+
+Revision ID: 3ddb5ee98a50
+Revises: 4864575a8ef9
+Create Date: 2015-05-18 09:23:54.858400
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3ddb5ee98a50'
+down_revision = '4864575a8ef9'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
