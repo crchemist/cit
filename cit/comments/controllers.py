21341e1d261dc908b0cac389a9b3193d173f14c2
Olexbaiko <olexbajk@gmail.com>
2015-05-19 11:59:37 +0300

    t_115:add api for removing comment

--- cit/comments/controllers.py
+++ cit/comments/controllers.py
@@ -1,3 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, session, jsonify
+
+from .models import Comment
+from ..db import db
 
 comments_bp = Blueprint('comments', __name__)
+
+
+class DeleteRequest:
+    def delete_comment(self, id):
+        comment = Comment.query.get_or_404(int(id))
+        db.session.delete(comment)
+        db.session.commit()
+
+    def get_comment(self, id):
+        return db.session.query(Comment).get(int(id))
+
+
+@comments_bp.route('/comments/<comment_id>/', methods=['DELETE'])
+def deleted_comment(comment_id):
+    dr = DeleteRequest()
+    delete = dr.get_comment(comment_id)
+    if delete is None:
+        response = jsonify({'status': 'Not found'})
+        response.status = 404
+        return response
+    dr.delete_comment(comment_id)
+    return jsonify({'status': 'OK'})
