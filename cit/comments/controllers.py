561c3115be1bfb61fca60e6f5704ab98a152c94a
Olexbaiko <olexbajk@gmail.com>
2015-05-19 21:59:17 +0300

    t_115:add api for removing comment fixed

--- cit/comments/controllers.py
+++ cit/comments/controllers.py
@@ -7,25 +7,12 @@ from ..db import db
 comments_bp = Blueprint('comments', __name__)
 
 
-class DeleteRequest:
-    def delete_comment(self, id):
-        comment = Comment.query.get_or_404(int(id))
-        permission = User.is_superuser or User.id == Comment.id
-        if permission:
-            db.session.delete(comment)
+@comments_bp.route('/comments/<int:comment_id>/', methods=['DELETE'])
+def delete_comment(comment_id):
+    comment_query = db.session.query(Comment)
+    comment_filtered = comment_query.filter(Comment.id == comment_id)
+    permission = User.is_superuser or User.id == Comment.id
+    if permission:
+        comment_filtered.delete()
         db.session.commit()
-
-    def get_comment(self, id):
-        return db.session.query(Comment).get(int(id))
-
-
-@comments_bp.route('/comments/<comment_id>/', methods=['DELETE'])
-def deleted_comment(comment_id):
-    dr = DeleteRequest()
-    delete = dr.get_comment(comment_id)
-    if delete is None:
-        response = jsonify({'status': 'Not found'})
-        response.status = 404
-        return response
-    dr.delete_comment(comment_id)
-    return jsonify({'status': 'OK'})
+        return jsonify({'status': 'OK'})
