bc5d9893d5d0ad7987e1120f9038460e760ae4af
Olexbaiko <olexbajk@gmail.com>
2015-05-20 18:44:47 +0300

    add api for removing comment with permissions

--- cit/comments/controllers.py
+++ cit/comments/controllers.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, session, jsonify
+from flask import Blueprint, session, jsonify, g
 
 from .models import Comment
-from ..auth.models import User
 from ..db import db
 
 comments_bp = Blueprint('comments', __name__)
@@ -9,10 +8,15 @@ comments_bp = Blueprint('comments', __name__)
 
 @comments_bp.route('/comments/<int:comment_id>/', methods=['DELETE'])
 def delete_comment(comment_id):
-    comment_query = db.session.query(Comment)
-    comment_filtered = comment_query.filter(Comment.id == comment_id)
-    permission = User.is_superuser or User.id == Comment.id
-    if permission:
-        comment_filtered.delete()
-        db.session.commit()
-        return jsonify({'status': 'OK'})
+    comment_filtered = db.session.query(Comment).filter(Comment.id == comment_id)
+    has_permission = g.user.is_superuser or g.user.id == Comment.author_id
+    if has_permission:
+        result = comment_filtered.delete()
+        if result:
+            db.session.commit()
+            return jsonify({'status': 0})
+        else:
+            db.session.rollback()
+            return jsonify({'msg': 'comment not found', 'status': 1})
+    else:
+        return jsonify({'status': 1}), 405
