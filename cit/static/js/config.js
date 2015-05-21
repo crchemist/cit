3b83d4b81a015fe6fca4703d8d1b273b9e88b3c2
zimoham <zimoham@gmail.com>
2015-05-21 17:15:00 +0300

    fixed gallery

--- cit/static/js/config.js
+++ cit/static/js/config.js
@@ -2,15 +2,12 @@ app.config(
 	function ($routeProvider) {
 		$routeProvider
 			.when('/profile', {
-				templateUrl: 'static/templates/profile.html'
-			})
-			
-			.when('/', {
-				templateUrl: 'static/templates/context.html'
-			})
+				templateUrl: 'static/templates/profile.html'		
+			})			
 			.when('/report-issue/', {
 				templateUrl: 'static/templates/report_issue.html',
 				controller: 'UserNS'
 			});			
 	}
 )
+
