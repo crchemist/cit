37a97b29eaa67eec0011638a2f8d9164e1e22ec8
Olexbaiko <olexbajk@gmail.com>
2015-05-21 11:37:26 +0300

    t_115:add api for removing comments

--- cit/comments/controllers.py
+++ cit/comments/controllers.py
@@ -3,6 +3,7 @@ from flask import Blueprint, session, jsonify, g
 from .models import Comment
 from ..auth.models import User
 from ..db import db
+from sqlalchemy import or_
 
 comments_bp = Blueprint('comments', __name__)
 
@@ -11,9 +12,8 @@ comments_bp = Blueprint('comments', __name__)
 def delete_comment(comment_id):
     comment_query = db.session.query(Comment)
     comment_filtered = comment_query.filter(Comment.id == comment_id)
-    owner = comment_query.join(User).filter(Comment.author_id == User.id).\
-        filter(Comment.author_id == g.user.id).all()
-    has_permission = g.user.is_superuser or owner
+    has_permission = comment_query.join(User).filter(Comment.author_id == User.id).\
+        filter(or_(Comment.author_id == g.user.id, g.user.is_superuser is True))
     if has_permission:
         result = comment_filtered.delete()
         if result:
@@ -24,3 +24,4 @@ def delete_comment(comment_id):
             return jsonify({'msg': 'comment not found', 'status': 1})
     else:
         return jsonify({'status': 2}), 405
+
