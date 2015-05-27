74aa2611962a962443d114611f5f570f0573ed97
IvannaBesarab <ibesarab1993@gmail.com>
2015-05-28 01:23:02 +0300

    imported db from cit and connected models and database to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -2,10 +2,21 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from cit.db import db
+from cit import create_app
+app = create_app()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# set the sqlalchemy url to the one defined for flask-sqlalchemy
+config.set_main_option('sqlalchemy.url', app.config['SQLALCHEMY_DATABASE_URI'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -15,13 +26,12 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = db.Model.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
