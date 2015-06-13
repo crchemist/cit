32d773a3d70d2b0f4b1e1bbce009c36bc8006f37
bubanoid <an.andrusyk@gmail.com>
2015-06-13 19:34:37 +0300

    single quotes were used instead double quotes

--- cit/issues/models.py
+++ cit/issues/models.py
@@ -4,11 +4,11 @@ from geoalchemy2 import Geography
 
 class Issue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    reporter = db.Column(db.Integer, db.ForeignKey("user.id"))
+    reporter = db.Column(db.Integer, db.ForeignKey('user.id'))
     description = db.Column(db.String(120))
     coordinates = db.Column(Geography(geometry_type='GEOMETRY'))
 
-    def __init__(self, description="", coordinates="", reporter=None):
+    def __init__(self, description='', coordinates='', reporter=None):
         self.description = description
         self.coordinates = coordinates
         self.reporter = reporter
@@ -16,11 +16,11 @@ class Issue(db.Model):
 
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    issue_id = db.Column(db.Integer, db.ForeignKey("issue.id"))
+    issue_id = db.Column(db.Integer, db.ForeignKey('issue.id'))
     file_path = db.Column(db.String(200))
 
-    issue = db.relationship("Issue")
+    issue = db.relationship('Issue')
 
-    def __init__(self, issue="", file_path=""):
+    def __init__(self, issue='', file_path=''):
         self.issue = issue
         self.file_path = file_path
